bd810710cff712bd1f60352f546908c8ecd92893
michaelbest55 <michaelbest55@users.noreply.github.com>
2019-10-08 08:37:20 +0200

    TODO connect to spark remote

--- basic_hdfs/src/main/scala/basic_spark_output/hive_tables.scala
+++ basic_hdfs/src/main/scala/basic_spark_output/hive_tables.scala
@@ -20,12 +20,28 @@ object Hdfs extends App{
     fs.close()
   }
   
+  // Write files to HFDS on the HDP sandbox
   //Hdfs.write("hdfs://sandbox-hdp.hortonworks.com:8020", "hdfs://sandbox-hdp.hortonworks.com:8020/user/anonymous/drivers.csv", "src/main/resources/data-spark/drivers.csv")
   //Hdfs.write("hdfs://sandbox-hdp.hortonworks.com:8020", "hdfs://sandbox-hdp.hortonworks.com:8020/user/anonymous/timesheet.csv", "src/main/resources/data-spark/timesheet.csv")
   //Hdfs.write("hdfs://sandbox-hdp.hortonworks.com:8020", "hdfs://sandbox-hdp.hortonworks.com:8020/user/anonymous/truck_event_text_partition.csv", "src/main/resources/data-spark/truck_event_text_partition.csv")   
+  
+ //Create Spark context
+ /*TODO: Set the HADOOP_CONF_DIR and all the necessary files to connect to the environment running in the HDP sandbox 
+   as seen in https://stackoverflow.com/a/38483127/11807319 :
+   fs.defaultFS (in case you intent to read from HDFS)
+   dfs.nameservices
+   yarn.resourcemanager.hostname or yarn.resourcemanager.address
+   yarn.application.classpath
+   core-site.xml
+   yarn-site.xml
+   hdfs-site.xml
+   mapred-site.xml
+*/
+ System.setProperty("HADOOP_CONF_DIR")=??
+ val spark = org.apache.spark.sql.SparkSession.builder.master("yarn").deployMode("client").appName("Spark CSV Reader").getOrCreate;
+ //val spark = org.apache.spark.sql.SparkSession.builder.master("spark://sandbox-hdp.hortonworks.com:4040").appName("Spark CSV Reader").getOrCreate;
 
-  //val spark = org.apache.spark.sql.SparkSession.builder.master("spark://sandbox-hdp.hortonworks.com:4040").appName("Spark CSV Reader").getOrCreate;
-  val spark = org.apache.spark.sql.SparkSession.builder.master("yarn").deployMode("client").appName("Spark CSV Reader").getOrCreate;
-  //val df = spark.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load("src/main/resources/data-spark/drivers.csv")
+ // Start operating on the data
+ //val df = spark.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load("src/main/resources/data-spark/drivers.csv")
 
 }
