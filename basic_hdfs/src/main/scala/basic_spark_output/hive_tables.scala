88b62d7d2464bf027a9a6a7ccdf23e47a9c29317
michaelbest55 <michaelbest55@hotmail.com>
2019-10-10 22:36:42 +0200

    Update scala app

--- basic_hdfs/src/main/scala/basic_spark_output/hive_tables.scala
+++ basic_hdfs/src/main/scala/basic_spark_output/hive_tables.scala
@@ -1,8 +1,11 @@
-
 // This code is meant to upload files to the HDFS
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import scala.io.Source
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+import java.io.File
+
 
 object Hdfs extends App{
 
@@ -25,23 +28,29 @@ object Hdfs extends App{
   //Hdfs.write("hdfs://sandbox-hdp.hortonworks.com:8020", "hdfs://sandbox-hdp.hortonworks.com:8020/user/anonymous/timesheet.csv", "src/main/resources/data-spark/timesheet.csv")
   //Hdfs.write("hdfs://sandbox-hdp.hortonworks.com:8020", "hdfs://sandbox-hdp.hortonworks.com:8020/user/anonymous/truck_event_text_partition.csv", "src/main/resources/data-spark/truck_event_text_partition.csv")   
   
- //Create Spark context
- /*TODO: Set the HADOOP_CONF_DIR and all the necessary files to connect to the environment running in the HDP sandbox 
-   as seen in https://stackoverflow.com/a/38483127/11807319 :
-   fs.defaultFS (in case you intent to read from HDFS)
-   dfs.nameservices
-   yarn.resourcemanager.hostname or yarn.resourcemanager.address
-   yarn.application.classpath
-   core-site.xml
-   yarn-site.xml
-   hdfs-site.xml
-   mapred-site.xml
-*/
- System.setProperty("HADOOP_CONF_DIR")=??
- val spark = org.apache.spark.sql.SparkSession.builder.master("yarn").deployMode("client").appName("Spark CSV Reader").getOrCreate;
- //val spark = org.apache.spark.sql.SparkSession.builder.master("spark://sandbox-hdp.hortonworks.com:4040").appName("Spark CSV Reader").getOrCreate;
 
- // Start operating on the data
- //val df = spark.read.format("csv").option("header", "true").option("mode", "DROPMALFORMED").load("src/main/resources/data-spark/drivers.csv")
 
+  // Create Spark Session
+  val warehouseLocation = new File("spark-warehouse").getAbsolutePath
+  val spark = SparkSession.builder().appName("driver data").master("local").config("spark.sql.warehouse.dir", warehouseLocation).enableHiveSupport().getOrCreate()
+  spark.sql("set spark.sql.caseSensitive=true")   
+
+  // load csv files as Spark DataFrames 
+  val df = spark.read.option("header",true).csv("hdfs:///user/anonymous/drivers.csv")
+  val df2 = spark.sql("SELECT * FROM csv.`hdfs:///user/anonymous/timesheet.csv`")
+  //val df2 = spark.read.option("header",true).csv("hdfs:///user/anonymous/timesheet.csv")
+  //val df3 = spark.read.option("header","true").option("maxColumns", 25000).schema(customSchema).csv("hdfs:///user/anonymous/truck_event_text_partition.csv")
+  //val df3 = spark.sql("SELECT * FROM csv.`hdfs:///user/anonymous/truck_event_text_partition.csv`")
+
+  // Save the tables in Hive
+  df.createOrReplaceTempView("driversTempTable")
+  spark.sqlContext.sql("create table driversTable4 as select * from driversTempTable")
+  df2.createOrReplaceTempView("timeSheetTempTable")
+  spark.sqlContext.sql("create table timeSheetTable4 as select * from timeSheetTempTable")
+  //df3.createOrReplaceTempView("truckeventTempTable")
+  //spark.sqlContext.sql("create table truckEventTable as select * from truckEventTempTable")
+  
+  // Join hive tables
+  spark.sql("SELECT timeSheetTable4.driverId, name, `hours-logged`, `miles-logged`  FROM timeSheetTable4 JOIN driversTable4 WHERE driversTable4.driverId=timeSheetTable4.driverId").show()
 }
+
